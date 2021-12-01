a4bfd0c117ce5ea9fd68712a23208f68b84cb013
Kevin Su <kevinsu@outlook.co.nz>
2021-12-01 19:08:27 +1300

    properties

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
 function App() {
+  
   return (
     <div className="custom-back">
       <h1>Houdini stuffs</h1>
       <div className = "container"></div>
+      <div className = "container2">hiu</div>
+      <div className = "container3"></div>
+      <div className = "container4"></div>
+      <h1 className = "fancy-header">Hello</h1>
     </div>
   );
 }
---
+++ src/csspaint.js
@@ -0,0 +1,6 @@
+window.CSS.registerProperty({
+    name: "--my-yellow",
+    syntax: "<color>",
+    initialValue: 'yellow',
+    inherits: false
+});
